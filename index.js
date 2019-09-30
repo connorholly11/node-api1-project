fa4e462a893d7860882c74a0d46c45618aef57cb
Connor Holly <connorholly11@gmail.com>
2019-09-30 12:43:18 -0400

    api up and running, dependencies installed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("HELLO WORLD");
+});
+
+server.listen(5000, () => {
+  console.log("API up and running on port 5000");
+});
