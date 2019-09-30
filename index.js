55eebe99327f15724211ee37245f4a1cf7ddb57a
Connor Holly <connorholly11@gmail.com>
2019-09-30 15:03:46 -0400

    req.body is undefined

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ server.get("/", (req, res) => {
   res.send("HELLO WORLD from node api project");
 });
 
-//post users - GETTING A 400
+//post users - GETTING A 400, but I think it's working
 server.post("/users", (req, res) => {
   const newUser = req.body;
 
@@ -86,13 +86,27 @@ server.delete("/users/:id", (req, res) => {
 
 server.put("/users/:id", (req, res) => {
   const id = req.params.id;
-});
+  const changes = req.body;
+  console.log("id", id);
+  console.log("changes", changes);
 
-// POST	/api/users	Creates a user using the information sent inside the request body.
-// GET	/api/users	Returns an array of all the user objects contained in the database.
-// GET	/api/users/:id	Returns the user object with the specified id.
-// DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.
-// PUT	/api/users/:id	Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+  db.update(id, changes)
+    .then(updated => {
+      if (updated) {
+        res.json(updated);
+      } else {
+        res.status(404).json({
+          message: "404 not found updating"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: "ALERT, THERE IS AN ERROR - put /users/:id"
+      });
+    });
+});
 
 server.listen(5000, () => {
   console.log("API up and running on port 5000");
